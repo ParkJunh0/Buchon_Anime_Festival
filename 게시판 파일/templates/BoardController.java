7b5509849ce61aa0b0cffcd8934712bf034aba5a
soSozz <88712895+soSozz@users.noreply.github.com>
2023-03-07 15:21:14 +0900

    자유게시판, Q&A, 공지사항(html,image,controller)
    수정본 자유게시판, Q&A, 공지사항(html,image,controller)

---
+++ 게시판 파일/templates/BoardController.java
@@ -0,0 +1,49 @@
+package com.biaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="/ko")
+public class BoardController {
+    @GetMapping(value="/notice")
+    public String notice(){
+        return "/Notice/notice";
+    }
+    @GetMapping(value="/notice/detail")
+	public String noticedetail(){
+		return "/Notice/noticedetail";
+	}
+	@GetMapping(value="/notice/form")
+	public String noticeform(){
+		return "/Notice/noticeform";
+	}
+
+    @GetMapping(value="/qna")
+    public String qna(){
+        return "/QnA/qna";
+        
+    }@GetMapping(value="/qna/detail")
+    public String qnadetail(){
+        return "/QnA/qnadetail";
+    }
+    
+    @GetMapping(value="/qna/form")
+    public String qnaform(){
+        return "/QnA/qnaform";
+    }
+    @GetMapping(value="/freeboard")
+    public String freeboard(){
+        return "/FreeBoard/freeboard";
+    }
+	@GetMapping(value="/freeboard/detail")
+	public String freeboarddetail(){
+		return "/FreeBoard/freeboarddetail";
+	}
+	@GetMapping(value="/freeboard/form")
+	public String freeboardform(){
+		return "/FreeBoard/freeboardform";
+	}
+	
+}
