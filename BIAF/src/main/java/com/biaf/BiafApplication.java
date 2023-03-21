0c419f67b4ad145f7058ef7d982d05a9c1788c7c
hb0623 <whb2359@naver.com>
2023-03-21 17:20:56 +0900

    굿즈 부분
    굿즈 등록 통합, 굿즈 페이지 제작, 주석 추가
    (Goods.html, .css, .js)
    (BIAFController.java)

--- BIAF/src/main/java/com/biaf/BiafApplication.java
+++ BIAF/src/main/java/com/biaf/BiafApplication.java
@@ -2,6 +2,9 @@ package com.biaf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 
 @SpringBootApplication
 public class BiafApplication {
@@ -9,5 +12,9 @@ public class BiafApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BiafApplication.class, args);
 	}
-
+	
+	@Bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+		return new HiddenHttpMethodFilter();
+	}
 }
--- BIAF/src/main/java/com/biaf/entity/Goods.java
+++ BIAF/src/main/java/com/biaf/entity/Goods.java
@@ -1,10 +1,17 @@
 package com.biaf.entity;
 
-import java.time.LocalDateTime;
-
-import org.springframework.data.annotation.Id;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
 import com.biaf.constant.GoodsSellStatus;
+import com.biaf.dto.GoodsFormDto;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -12,14 +19,30 @@ import lombok.ToString;
 
 @Getter
 @Setter
+@Table(name="goods")
 @ToString
-public class Goods {
+@Entity
+public class Goods extends baseEntity {
 
 	@Id
-	private Long id;	//상품 코드  private String itemNm; //상품명  private int price; //가격
-	private int stockNumber; //재고수량  private String itemDetail; //상품 상세 설명
-	private GoodsSellStatus itemSellStatus; //상품 판매 상태
-	private LocalDateTime regTime; // 등록시간
-	private LocalDateTime updateTime; // 수정시간	
-}
+    @Column(name = "goods_id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "goods_seq")
+    @SequenceGenerator(name = "goods_seq", sequenceName = "goods_seq", allocationSize = 1)
+    private Long id;	//상품 코드  
+	private String goodsNm; //상품명  
+	private int price; //가격
+	private int stockNumber; //재고수량  
+	private String goodsDetail; //상품 상세 설명	
+
 	
+	@Enumerated(EnumType.STRING)
+	private GoodsSellStatus goodsSellStatus; //상품 판매 상태
+
+	public void updateGoods(GoodsFormDto goodsFormDto){
+		this.goodsNm = goodsFormDto.getGoodsNm();
+		this.price = goodsFormDto.getPrice();
+		this.stockNumber = goodsFormDto.getStockNumber();
+		this.goodsDetail = goodsFormDto.getGoodsDetail();
+		this.goodsSellStatus = goodsFormDto.getGoodsSellStatus();
+	}
+}
