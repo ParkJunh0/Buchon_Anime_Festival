9aaaab50ec2d60148cd166fe6a969c54a93d619d
hb0623 <whb2359@naver.com>
2023-03-28 09:36:10 +0900

    Revert: 복구
    잘못 올렸다해서 리버트가 오류나서 아예 다시 올립니다.

--- BIAF/src/main/java/com/biaf/repository/MemberRepository.java
+++ BIAF/src/main/java/com/biaf/repository/MemberRepository.java
@@ -11,7 +11,8 @@ public interface MemberRepository extends JpaRepository<Member, Long>{
 
    
    Member findByMemberEmail(String memberEmail);
-   boolean existsByMemberEmail(String memberEmail);
+
+	boolean existsByMemberEmail(String memberEmail);
    void deleteByMemberEmail(String memberEmail);
 
 }
